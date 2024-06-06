0fe650494a95086c83764c80caee79a24898f8c5
murugan-dev <murugankumarasamy897@gmail.com>
2024-06-06 10:49:02 -0400

    Components Learning

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup>
+    import NestedComponent from './NestedComponent.vue';
+    console.log("Hello world");
+    
+</script>
+
+<template>
+    <div>
+        <h1>HELLO WORLD</h1>
+        <NestedComponent />
+    </div>
+    
+</template>
+
+<style scoped>
+    h1{
+        color: red;
+    }
+</style>
