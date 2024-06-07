08aa4431b64967ab5c15100974e69c36450ee296
murugan-dev <murugankumarasamy897@gmail.com>
2024-06-06 21:23:20 -0400

    learned events

--- src/components/MyEvents.vue
+++ src/components/MyEvents.vue
@@ -1,7 +1,8 @@
 <script setup>
 
 // FOR CLICKING EVENT
-    import {ref} from "vue";
+    import {ref, watch} from "vue";
+    import { debounce } from 'lodash-es';
 
     let count = ref(0);
     let firstName = ref("");
@@ -22,19 +23,47 @@
         console.log(e.target.value)
     }
 
-    // FOR ON SUBMIT EVENT
+// FOR ON SUBMIT EVENT
+const debouncedFirstName = ref('');
+const debouncedLastName = ref('');
 
-    const handleSubmit = (e)=>{
-        e.preventDefault();
-        
-    }
+const updateFirstName = debounce((value) => {
+  firstName.value = value;
+}, 300);
+
+const updateLastName = debounce((value) => {
+  lastName.value = value;
+}, 300);
+
+watch(debouncedFirstName, (newValue) => {
+  updateFirstName(newValue);
+});
+
+watch(debouncedLastName, (newValue) => {
+  updateLastName(newValue);
+});
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  // Now firstName and lastName contain the debounced values from the input fields
+  console.log('First Name:', firstName.value);
+  console.log('Last Name:', lastName.value);
+  // You can now use the firstName and lastName values as needed
+};
+
+
+//FUNCTION CALLING
+
+function showMessage(msg){
+    alert(msg);
+}
+
 
 </script>
 
 <template>
     <div>
         <h1>CLICK EVENT </h1>
-
         <h3>count: {{ count }}</h3>
         <button @click="increment">increment</button>
         <button @click="decrement">decrement</button>
@@ -46,15 +75,19 @@
 
         <h1>ONSUBMIT EVENT</h1>
         <form action="" @submit="handleSubmit">
-            <input type="text" placeholder="Enter your name" v-modal="firstName"/>
-            <input type="text" placeholder="Enter your Last name" v-modal="lastName"/>
+            <input type="text" placeholder="Enter your name" v-model="firstName"/>
+            <input type="text" placeholder="Enter your Last name" v-model="lastName"/>
             <button type="submit">submit</button>
         </form>
 
         <h3>your frist name: {{ firstName }}</h3>
         <h3>your last name: {{ lastName }}</h3>
 
+        <hr>
+        
+        <h1>function Calling</h1>
 
+        <button @click="showMessage('This is my message')">show Message</button>
 
     </div>
 </template>
