db0ec3224ae36b82221946323953bbbe61f24e88
murugan-dev <murugankumarasamy897@gmail.com>
2024-07-01 11:57:59 -0400

    create circle Application completed

--- src/components/simpleProblem/CircleApplication.vue
+++ src/components/simpleProblem/CircleApplication.vue
@@ -1,19 +1,40 @@
 <script setup>
 import { ref } from 'vue';
-import "../../style.css";
 
 let val = ref(0);
+let findHeightAndWidth = ref(0);
 
 function handleSubmit(e) {
     e.preventDefault();
     const inputVal = e.target.querySelector('input').value;
     val.value = parseInt(inputVal);
     console.log(val.value);
+    findHeightAndWidth.value = 200 / val.value;
+    handleContainer(findHeightAndWidth.value);
+}
+
+function handleContainer(value) {
+    const container = document.querySelector('#box');
+    container.innerHTML = "";  
+    console.log(value);
+    for (let i = val.value; i > 0; i--) {
+        let div = document.createElement('div');
+        // div.className = "inner";
+        div.style.height = `${i * value}px`;
+        div.style.width = `${i * value}px`;
+        div.style.position = "absolute";
+        // div.style.top = `${(200 - i * value) / 2}px`;
+        // div.style.left = `${(200 - i * value) / 2}px`;
+        div.style.borderRadius = "50%";
+        div.style.border = "1px solid #252525";
+        container.appendChild(div);
+    }
+    console.log(container);
 }
 </script>
 
 <template>
-    <div class="flex justify-center items-center flex-col lg:flex-row bg-gray-200 gap-5 h-full w-full px-4">
+    <div class="flex justify-center items-center flex-col lg:flex-row bg-gray-200 gap-5 h-full w-full px-4 py-5">
         <div class="bg-white shadow-2xl rounded-md h-1/2 p-10 w-4/5 flex justify-center items-center flex-col">
             <h1 class="text-2xl text-center font-bold">Inputs</h1>
             <form @submit="handleSubmit" class="shadow-2xl border-[1px] flex flex-col justify-center items-center gap-2 p-5 w-4/5 rounded-lg">
@@ -28,11 +49,11 @@ function handleSubmit(e) {
         </div>
         <div class="bg-white shadow-2xl rounded-md h-1/2 p-10 w-4/5 flex justify-center items-center flex-col">                                                                                            
             <h1 class="text-center text-2xl font-bold">Output</h1>
-            <div class="h-[200px] w-[200px] border-[1px] border-black"></div>
+            <div class="relative h-[200px] w-[200px] border-[1px] border-black flex justify-center items-center" id="box"></div>
         </div>
     </div>
 </template>
 
 <style scoped>
-/* Additional styling if needed */
+
 </style>
